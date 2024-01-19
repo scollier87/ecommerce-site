b764de6c07186dcb2e651f617b8756635a0fc7d7
SeanyC <sean.collier928@yahoo.com>
2024-01-19 11:02:35 -0800

    renamed component

--- ecommerce-site/src/App.js
+++ ecommerce-site/src/App.js
@@ -1,14 +1,14 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
+import NavBar from './components/NavBar/NavBar';
 import Home from './components/Splash/Splash';
 // ... other imports
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/shop" element={<Shop />} />
