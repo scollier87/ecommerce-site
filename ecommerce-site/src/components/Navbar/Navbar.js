e164c6e66fbfd9b0eee925f3bad463846d0fb46b
SeanyC <sean.collier928@yahoo.com>
2024-01-19 10:39:55 -0800

    splash/navbar skeleton

---
+++ ecommerce-site/src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-logo">
+                <Link to="/">RockClimbingNeeds</Link>
+            </div>
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/shop">Shop</Link>
+                <Link to="/cart">Cart</Link>
+                <Link to="/login">Login</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ ecommerce-site/src/components/Splash/Splash.js
@@ -0,0 +1,14 @@
+// src/components/Home/Home.js
+import React from 'react';
+import './Splash.css'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Rock Climbing needs</h1>
+            <p>“The best part of climbing is when it all clicks and gravity ceases to exist.” – Chris Sharma</p>
+        </div>
+    );
+}
+
+export default Home;
