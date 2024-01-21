9f4be718ba3cdfe51a00dcc8843ed120f0c7b3dc
SeanyC <sean.collier928@yahoo.com>
2024-01-21 14:10:15 -0800

    updated fetch url

--- ecommerce-site/src/components/utils/cartUtils.js
+++ ecommerce-site/src/components/utils/cartUtils.js
@@ -1,5 +1,5 @@
 export const fetchUserCart = async (userId) => {
-    const response = await fetch(`https://ecommerce-site-bae1b-default-rtdb.firebaseio.com/Users/${userId}/cart.json`);
+    const response = await fetch(`https://ecommerce-site-bae1b-default-rtdb.firebaseio.com/data/Users/${userId}/cart.json`);
     if (!response.ok) {
         throw new Error('Failed to fetch cart data');
     }
